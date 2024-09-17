6bff824c81812f0be4e91a811e5c4b480b8b1af3
rinatyuk-git <17_ufo_RB@proton.me>
2024-09-17 18:05:41 -0400

    given Permissions

--- materials/views.py
+++ materials/views.py
@@ -1,32 +1,54 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
 from materials.serializers import (CourseSerializer, LessonSerializer,
                                    CourseDetailSerializer
                                    )
+from users.permissions import (
+    IsOwner,
+    IsModerator
+)
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated | ~IsModerator,)
+    # permission_classes = (~IsModerator,)
+
+    def perform_create(self, serializer):
+        new_lesson = serializer.save()
+        new_lesson.owner = self.request.user
+        new_lesson.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
+    # queryset = Lesson.objects.all()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsModerator | IsOwner,)
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsModerator | IsOwner,)
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (~IsModerator | IsOwner)
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -36,3 +58,17 @@ class CourseViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return CourseDetailSerializer
         return CourseSerializer
+
+    def perform_create(self, serializer):
+        new_course = serializer.save()
+        new_course.owner = self.request.user
+        new_course.save()
+
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = (~IsModerator,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModerator | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (~IsModerator | IsOwner,)
+        return super().get_permissions()
